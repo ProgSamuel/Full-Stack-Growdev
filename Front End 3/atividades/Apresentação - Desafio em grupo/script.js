d0883ac290fbefb5dcded59685ec695489c993b4
Samuel_Ramos <progsamu@gmail.com>
2023-08-19 17:12:31 -0300

    add - new style

---
+++ Front End 3/atividades/Apresentação - Desafio em grupo/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('#home', {duration: 1000})
+
+sr.reveal('#descricao', {
+    duration: 1000
+})
+
+sr.reveal('#elenco', {
+    duration: 1000
+})
